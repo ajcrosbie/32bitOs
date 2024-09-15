24728a0b90c748305e60ec3cfeb5f271bf2d4832
ajcrosbie <aaron.j.crosbie@gmail.com>
2024-09-15 19:10:11 +0100

    somehow lowered threshold for overflow (i think div was the problem)

--- unconfirmedCode/code/kernel.cpp
+++ unconfirmedCode/code/kernel.cpp
@@ -3,27 +3,16 @@
 extern "C" {
     void constructTable();        // Initializes the scan code to ASCII lookup table
     void scans(char* buffer);     // Reads input into the buffer using scan codes
-    void* mallocator(); 
+    void* malocator(); 
 }
 
 
 
-int calcSquare(int x){
-    return x*x;
-}
 
 int numeric(char a){
     return (a<='9'&&'0'<=a);
 }
 
-int div (int a, int b){
-    int i=0;
-    while (a>=b){
-        i++;
-        a -=b;
-    }
-    return i;
-}
 
 int strToInt(char* str){
     int x = 0;
@@ -31,11 +20,12 @@ int strToInt(char* str){
     
     while (numeric(str[i])){
         x*=10;
-        x+=(int)str[i] - 48;
+        x+=(int) str[i] - '0';
         i++;
     }
     return x;
 }
+
 int pow(int a, int b){
     int x=1;
     while(b>0){
@@ -44,13 +34,16 @@ int pow(int a, int b){
     }
     return x;
 }
+
 int calcSpace(int a){
     int x=1;
-    while (a>=pow(10,x)){
+    while (a>=10){
+        a/=10;
         x+=1;
     }
     return x;
 }
+
 void loadStr(char* string, char* num){
     unsigned i=0;
     while (num[i] !='\0'){
@@ -58,46 +51,45 @@ void loadStr(char* string, char* num){
         i++;
     }
 }
+
 void print(char* string){
     char* vmem = (char*)0xb8000;
     static int i=0;
     int itter=0;
     while (string[itter] != '\0'){
         vmem[i*2] = string[itter];
-        // when trying this on the linux system it will segfault as 
-        // programs aren't allowed to access vmem directly
+        // when trying this on the linux system it wil segfault as 
+        // programs aren't alowed to access vmem directly
         vmem[i*2+1] = 0x07;
         i++;
         itter++;
     }
 }
-int toStr(char* string, int input){
-    unsigned i=0;
-    int value = strToInt(string);
+
+void toStr(char* string, int input){
+    int i=0;
     int divVal, mod, exp;
-    value = calcSquare(value);
-    int space = calcSpace(value);
+    int space = calcSpace(input);
     while (i!=space){
         exp = pow(10,space-i-1);
-        //printf("%d\n", exp);
-        divVal = div(value, exp);
-        value = value - divVal * exp;
+        divVal = input/exp;
+        input = input - divVal * exp;
         
-        string[i] = (char) (divVal+48);
+        string[i] = (char) (divVal+'0');
         i++;
     }
+    string[i] = 0;
 }
 
 
 extern "C" int main(){
     char buff[128];
-    buff[10] = 0;
-    // print("hello world");
     constructTable();
     scans(buff);
     print(buff);
     print(" squared is ");
     int usrInt = strToInt(buff);
-    toStr(buff, usrInt);
+    int value = usrInt*usrInt;
+    toStr(buff,value);
     print(buff);
 }
